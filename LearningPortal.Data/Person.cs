2d84fec57917a9dd141e9f4b855f38b2f744730b
Jonathan Tan <960166+kuyajon@users.noreply.github.com>
2026-04-16 20:46:51 +0800

    fix issue

--- LearningPortal.Data/Person.cs
+++ LearningPortal.Data/Person.cs
@@ -1,6 +1,6 @@
 public class Person
 {
-    public string Na me { get; set; }
+    public string Name { get; set; }
     public int Age { get; set; }
 
     public void Greet() => Console.WriteLine($"Hi, I'm {Name}.");
