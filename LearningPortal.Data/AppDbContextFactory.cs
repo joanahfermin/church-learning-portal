2b6cef8230bbc53761a23f3cd3c4f5fdc9e4d2f9
Joanah Fermin <ferminjoanah@gmail.com>
2026-04-17 13:59:49 +0800

    added AppDbContextFactory for migration

---
+++ LearningPortal.Data/AppDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningPortal.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseMySQL(configuration.GetConnectionString("DefaultConnection"));
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
--- LearningPortal.Data/Model/Client.cs
+++ LearningPortal.Data/Model/Client.cs
@@ -8,10 +8,10 @@ namespace LearningPortal.Data.Model
     //Represents a person's profile and learning identity. Linked one-to-one with User. All learning activity belongs to Client, not User.
     public class Client : AuditableEntity
     {
-        public long UsertId { get; set; }
+        public long UserId { get; set; }
         public string FullName { get; set; }
 
         public User User { get; set; }
-        ICollection<GroupMember> GroupMembers{ get; set; }
+        public ICollection<GroupMember> GroupMembers{ get; set; }
     }
 }
